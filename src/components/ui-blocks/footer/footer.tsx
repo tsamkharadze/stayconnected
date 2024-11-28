d38a340f36df08c5661a300496dbe0bcf07011eb
Sopiko Imnaishvili <sopho.imnaishvili@gmail.com>
2024-11-28 23:14:28 +0400

    add footer

---
+++ src/components/ui-blocks/footer/footer.tsx
@@ -0,0 +1,14 @@
+import ScreenLg from '@/components/page-containers/screen-lg';
+
+const Footer: React.FC = () => {
+  return (
+    <div className='border-border-soft border-t'>
+      <ScreenLg>
+        <div className='py-8'>
+          <p>Built by team 14. The source code is available on GitHub.</p>
+        </div>
+      </ScreenLg>
+    </div>
+  );
+};
+export default Footer;
