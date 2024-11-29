0f9fa1476b868df45e4bce2d78a7f4ef3bbc3dac
sopo <sofiko.imnaishvili@gmail.com>
2024-11-29 09:11:43 +0400

    add link to create question page

--- src/components/ui-blocks/header/components/controls.tsx
+++ src/components/ui-blocks/header/components/controls.tsx
@@ -1,13 +1,16 @@
 import { Button } from '@/components/ui/button';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 const Controls: React.FC = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   return (
     <div>
       {loggedIn ? (
         <div className='flex items-center gap-4'>
+          <Link to="createQuestion">
           <Button variant='default'>Add</Button>
+          </Link>
           <Avatar>
             <AvatarImage src='https://github.com/shadcn.png' />
             <AvatarFallback>CN</AvatarFallback>
