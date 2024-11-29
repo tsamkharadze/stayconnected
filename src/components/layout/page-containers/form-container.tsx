f15957d03b9f7310799d07411a4b1c9a1add04c0
sopo <sofiko.imnaishvili@gmail.com>
2024-11-29 23:50:35 +0400

    add input and textarea validations

--- src/components/layout/page-containers/form-container.tsx
+++ src/components/layout/page-containers/form-container.tsx
@@ -1,10 +1,8 @@
-import { PropsWithChildren } from "react";
-
-const FormContainer:React.FC<PropsWithChildren> =({children}) => {
-    return(
-        <form className='flex flex-col gap-8'>
-            {children}
-        </form>
-    )
+import { PropsWithChildren } from 'react';
+interface FormContainerProps {
+  onSubmit: React.FormEventHandler<HTMLFormElement>;
 }
+const FormContainer: React.FC<PropsWithChildren<FormContainerProps>> = ({ children, onSubmit }) => {
+  return <form className='flex flex-col gap-8' onSubmit={onSubmit}>{children}</form>;
+};
 export default FormContainer;
--- src/pages/create-question/views/create-question-page.tsx
+++ src/pages/create-question/views/create-question-page.tsx
@@ -5,25 +5,79 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
+import { useMutation } from '@tanstack/react-query';
+
+import { useForm } from 'react-hook-form';
+
+type FormFields = {
+  title: string;
+  description: string;
+  tags: Framework[];
+};
+type Framework = {
+  value: string;
+  label: string;
+};
+const sendQuestion = (data: FormFields) => {
+  return Promise.resolve(data);
+};
 
 const CreateQuestionPage = () => {
+  const { mutate: handleSendForm } = useMutation({
+    mutationFn: sendQuestion,
+    onSuccess: (data) => {
+      console.log('Question submitted successfully', data);
+    },
+    onError: (error) => {
+      console.error('Error submitting question', error);
+    },
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormFields>({
+    defaultValues: {
+      title: '',
+      description: '',
+      tags: [],
+    },
+  });
+
+  const title = register('title', {
+    required: 'Title should not be empty',
+  });
+  const description = register('description', {
+    required: 'Description should not be empty',
+  });
+
   return (
     <ScreenMd>
-      <div className="my-4">
-      <h1 className="text-2xl font-bold">Add Question</h1>
+      <div className='my-4'>
+        <h1 className='text-2xl font-bold'>Add Question</h1>
       </div>
-      <FormContainer>
+      <FormContainer
+        onSubmit={handleSubmit((data) => {
+          console.log('Form Submitted:', data);
+          handleSendForm(data);
+        })}
+      >
         <div>
           <Label htmlFor='title'>Title</Label>
-          <Input id='title'/>
+          <Input {...title} id='title' />
+          <p className='text-red-500'>{errors.description?.message}</p>
         </div>
         <div>
           <Label htmlFor='description'>Description</Label>
-          <Textarea id='description'/>
+          <Textarea {...description} id='description' />
+          <p className='text-red-500'>{errors.description?.message}</p>
         </div>
         <div>
+          <Label htmlFor='tags'>Tags</Label>
+
           <FancyMultiSelect />
         </div>
+
         <Button>Add question</Button>
       </FormContainer>
     </ScreenMd>
