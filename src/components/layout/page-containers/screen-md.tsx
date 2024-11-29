e8348f061d7d1c3176073c5fa321d305efacd7d0
sopo <sofiko.imnaishvili@gmail.com>
2024-11-29 12:37:47 +0400

    add medium container to hold content in 560px

--- src/components/layout/page-containers/screen-md.tsx
+++ src/components/layout/page-containers/screen-md.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 
 const ScreenMd: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className='mx-4 max-w-screen-lg sm:mx-12 md:mx-16 2xl:mx-auto'>
+    <div className='mx-4 sm:mx-12 md:mx-auto w-auto md:w-[560px]'>
       {children}
     </div>
   );
--- src/pages/create-question/views/create-question-page.tsx
+++ src/pages/create-question/views/create-question-page.tsx
@@ -1,8 +1,27 @@
+import ScreenMd from '@/components/layout/page-containers/screen-md';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Textarea } from '@/components/ui/textarea';
+
 const CreateQuestionPage = () => {
   return (
-    <>
-      <div>Create Question</div>
-    </>
+    <ScreenMd>
+      <div className="">
+      <h1>Add Question</h1>
+      <form className='flex flex-col gap-8'>
+        <div>
+          <Label htmlFor='title'>Title</Label>
+          <Input id='title' className='' />
+        </div>
+        <div>
+          <Label htmlFor='description'>Description</Label>
+          <Textarea id='description' className='' />
+        </div>
+        <Button className="">Add question</Button>
+      </form>
+      </div>
+    </ScreenMd>
   );
 };
 
