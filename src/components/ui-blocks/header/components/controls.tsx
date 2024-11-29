856c885c267fb8b418836a14be84e235b2d8b0d9
tsamkharadze <samkaradze@gmail.com>
2024-11-29 22:48:01 +0400

    ADD dropdown , EDIT header layout

--- src/components/ui-blocks/header/components/controls.tsx
+++ src/components/ui-blocks/header/components/controls.tsx
@@ -2,19 +2,50 @@ import { Button } from '@/components/ui/button';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { LogOut, Settings, User } from 'lucide-react';
 const Controls: React.FC = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   return (
     <div>
       {loggedIn ? (
         <div className='flex items-center gap-4'>
-          <Link to="createQuestion">
-          <Button variant='default'>Add</Button>
+          <Link to='createQuestion'>
+            <Button variant='default'>Add</Button>
           </Link>
-          <Avatar>
-            <AvatarImage src='https://github.com/shadcn.png' />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
+
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Avatar>
+                <AvatarImage src='https://github.com/shadcn.png' />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className='w-56' align='end' forceMount>
+              <DropdownMenuGroup>
+                <DropdownMenuItem>
+                  <Settings className='mr-2 h-4 w-4' />
+                  <span>User settings</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <User className='mr-2 h-4 w-4' />
+                  <span>Profile</span>
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem className='text-red-600'>
+                <LogOut className='mr-2 h-4 w-4' />
+                <span>Sign out</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       ) : (
         <div>
--- src/components/ui-blocks/header/views/header.tsx
+++ src/components/ui-blocks/header/views/header.tsx
@@ -1,26 +1,33 @@
 import ScreenLg from '@/components/layout/page-containers/screen-lg';
 import Logo from '../components/logo';
 import Controls from '../components/controls';
-import SearchBlock from '../components/search-block';
+import { Button } from '@/components/ui/button';
+import { Search } from 'lucide-react';
+import { Input } from '@/components/ui/input';
+
 const Header: React.FC = () => {
   return (
     <div className='border-border-soft border-b'>
       <ScreenLg>
-        <div className='hidden items-center justify-between gap-8 py-4 lg:flex'>
-          <Logo />
-          <SearchBlock />
-          <Controls />
-        </div>
-
-        <div className='flex flex-col items-center justify-between gap-8 py-4 lg:hidden'>
-          <div className='flex w-full items-center justify-between'>
+        <div className='grid grid-cols-2 grid-rows-[auto_auto] items-center gap-4 py-4 md:grid-cols-[auto_1fr_auto] md:grid-rows-1'>
+          <div className='col-span-1 md:col-start-1'>
             <Logo />
+          </div>
+
+          <div className='col-span-2 flex w-full items-center gap-4 md:col-span-1 md:col-start-2'>
+            <Input placeholder='Search...' />
+            <Button variant='secondary'>
+              <Search />
+            </Button>
+          </div>
+
+          <div className='col-span-1 col-start-2 row-start-1 flex justify-end md:col-start-3'>
             <Controls />
           </div>
-          <SearchBlock />
         </div>
       </ScreenLg>
     </div>
   );
 };
+
 export default Header;
