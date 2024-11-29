502c0662cdbeea6d4548a7d35d0a002a9ecca57a
sopo <sofiko.imnaishvili@gmail.com>
2024-11-29 12:49:09 +0400

    add create question containers

--- src/components/layout/page-containers/screen-md.tsx
+++ src/components/layout/page-containers/screen-md.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 
 const ScreenMd: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className='mx-4 sm:mx-12 md:mx-auto w-auto md:w-[560px]'>
+    <div className='mx-4 sm:mx-12 md:mx-auto w-auto md:w-[560px] my-8'>
       {children}
     </div>
   );
--- src/pages/create-question/views/create-question-page.tsx
+++ src/pages/create-question/views/create-question-page.tsx
@@ -7,8 +7,9 @@ import { Textarea } from '@/components/ui/textarea';
 const CreateQuestionPage = () => {
   return (
     <ScreenMd>
-      <div className="">
-      <h1>Add Question</h1>
+      <div className="my-4">
+      <h1 className="text-2xl font-bold">Add Question</h1>
+      </div>
       <form className='flex flex-col gap-8'>
         <div>
           <Label htmlFor='title'>Title</Label>
@@ -20,7 +21,6 @@ const CreateQuestionPage = () => {
         </div>
         <Button className="">Add question</Button>
       </form>
-      </div>
     </ScreenMd>
   );
 };
