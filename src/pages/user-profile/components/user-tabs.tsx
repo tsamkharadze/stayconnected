234ea09e8ccbca176dda733e14b99f4e7f6e8561
sopo <sofiko.imnaishvili@gmail.com>
2024-11-30 20:16:56 +0400

    add user tabs

---
+++ src/pages/user-profile/components/user-tabs.tsx
@@ -0,0 +1,18 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+const UserTabs:React.FC = () => {
+    return(
+        <div>
+            <Tabs defaultValue='questions' className='w-[400px]'>
+        <TabsList>
+          <TabsTrigger value='questions'>My Questions</TabsTrigger>
+          <TabsTrigger value='answers'>My Answers</TabsTrigger>
+        </TabsList>
+        <TabsContent value='questions'>
+          Make changes to your account here.
+        </TabsContent>
+        <TabsContent value='answers'>Change your password here.</TabsContent>
+      </Tabs>
+        </div>
+    )
+}
+export default UserTabs
--- src/pages/user-profile/views/user-profile-page.tsx
+++ src/pages/user-profile/views/user-profile-page.tsx
@@ -1,15 +1,19 @@
 import ScreenMd from '@/components/layout/page-containers/screen-md';
 import UserInfo from '../components/user-info';
+import UserTabs from '../components/user-tabs';
+
+
 const user = {
-  id: "123",
-  name: "John",
-  email: "John@mail.com",
-  rating: 7
-}
+  id: '123',
+  name: 'John',
+  email: 'John@mail.com',
+  rating: 7,
+};
 const UserProfile = () => {
   return (
     <ScreenMd>
-     <UserInfo user={user}/>
+      <UserInfo user={user} />
+      <UserTabs />
     </ScreenMd>
   );
 };
