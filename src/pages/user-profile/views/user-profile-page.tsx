878f3459584cf94ac291cd1045d9074c88108bfb
sopo <sofiko.imnaishvili@gmail.com>
2024-11-30 20:10:52 +0400

    destructure user info component

--- src/pages/user-profile/views/user-profile-page.tsx
+++ src/pages/user-profile/views/user-profile-page.tsx
@@ -1,36 +1,15 @@
 import ScreenMd from '@/components/layout/page-containers/screen-md';
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-
+import UserInfo from '../components/user-info';
+const user = {
+  id: "123",
+  name: "John",
+  email: "John@mail.com",
+  rating: 7
+}
 const UserProfile = () => {
   return (
     <ScreenMd>
-      <Card>
-        <CardHeader className='flex flex-row items-center gap-4'>
-          <div>
-            <Avatar className='size-12 md:size-24'>
-              <AvatarImage src='https://github.com/shadcn.png' />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-          </div>
-          <div>
-            <CardTitle>
-              <p className='truncate text-lg font-bold md:text-2xl'>
-                Staticemail@email.com
-              </p>
-            </CardTitle>
-
-            <CardDescription className='text-md'>
-              StaticRating :0
-            </CardDescription>
-          </div>
-        </CardHeader>
-      </Card>
+     <UserInfo user={user}/>
     </ScreenMd>
   );
 };
