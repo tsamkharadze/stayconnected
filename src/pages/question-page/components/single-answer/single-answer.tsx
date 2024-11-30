1c71698f86206155452fae6c3d06da9db1407e4a
richi1213 <datonanava11@gmail.com>
2024-11-30 12:57:42 +0400

    working on implementing accepted status on authorized user

--- src/pages/question-page/components/single-answer/single-answer.tsx
+++ src/pages/question-page/components/single-answer/single-answer.tsx
@@ -1,17 +1,21 @@
 import { Card, CardContent } from '@/components/ui/card';
 import { Toggle } from '@/components/ui/toggle';
-import { ThumbsUp, Check } from 'lucide-react';
+import { ThumbsUp, Check, BookmarkCheck } from 'lucide-react';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { SingleAnswerProps } from '@/pages/question-page/components/single-answer/signle-answer.types';
 import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
 
 const SingleAnswer: React.FC<SingleAnswerProps> = ({
-  username = 'Triapka',
-  date = '24 Nov 2013',
-  content = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi cum, ratione repudiandae cumque quaerat velit autem unde! Voluptatem sit quis tempora facilis! Quibusdam, obcaecati tempore totam ullam ad similique quaerat!',
-  votes = 1,
-  isAccepted = true,
+  username,
+  date,
+  content,
+  votes,
+  isAccepted,
 }) => {
+  const isAuthorized = true;
+  const isThereCorrectAnswerFromAnswers = false;
+
   return (
     <Card className='w-full border-none bg-background text-foreground'>
       <CardContent className='space-y-2 p-4'>
@@ -26,13 +30,10 @@ const SingleAnswer: React.FC<SingleAnswerProps> = ({
             </div>
             <span className='text-sm'>• {date}</span>
           </div>
-          {isAccepted && (
-            <Badge
-              variant='outline'
-              className='border-green-300 bg-green-100 text-green-800'
-            >
-              <Check className='mr-1 h-4 w-4' /> Accepted
-            </Badge>
+          {isAuthorized && !isThereCorrectAnswerFromAnswers && (
+            <Button size='icon' variant='secondary'>
+              <BookmarkCheck />
+            </Button>
           )}
         </div>
 
