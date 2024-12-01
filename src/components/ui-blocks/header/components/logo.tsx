65d15224d779447ed9d453414302e861972fd4c3
sopo <sofiko.imnaishvili@gmail.com>
2024-12-01 11:38:11 +0400

    add link to homepage

--- src/components/ui-blocks/header/components/logo.tsx
+++ src/components/ui-blocks/header/components/logo.tsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Logo: React.FC = () => {
   return (
     <div>
+      <Link to="/">
       <h1 className='text-onSurface text-sm font-bold md:text-2xl'>
         Stay connected
       </h1>
+      </Link>
     </div>
   );
 };
