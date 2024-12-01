82e0eece481b678b0e73284d2fcea3f6ed1d0d6c
sopo <sofiko.imnaishvili@gmail.com>
2024-12-01 11:42:43 +0400

    fix logo size

--- src/components/ui-blocks/header/components/logo.tsx
+++ src/components/ui-blocks/header/components/logo.tsx
@@ -4,7 +4,7 @@ const Logo: React.FC = () => {
   return (
     <div>
       <Link to="/">
-      <h1 className='text-onSurface text-sm font-bold md:text-2xl'>
+      <h1 className='text-onSurface text-xl font-bold'>
         Stay connected
       </h1>
       </Link>
