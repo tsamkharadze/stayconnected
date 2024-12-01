cff61018c71cf14ac55da0366bba13845e6f0ca9
Ani Jujunashvili <ana.jujunashvili@gmail.com>
2024-12-02 03:05:20 +0400

    get questions from db

--- src/pages/home-page/views/home-page.tsx
+++ src/pages/home-page/views/home-page.tsx
@@ -1,18 +1,18 @@
-import HomePageContainer from '@/components/layout/page-containers/home-page-container';
 import QuestionsList from './questions-list.tsx';
 import UserRating from './rating.tsx';
+import ScreenLg from '@/components/layout/page-containers/screen-lg.tsx';
 
 const HomePage = () => {
   return (
     <>
-      <HomePageContainer>
-        <div className='p-4 md:w-2/3 md:p-2 lg:p-4'>
-          <QuestionsList />
-        </div>
-        <div className='p-4 md:w-1/3 md:p-2 lg:p-4'>
+      <ScreenLg>
+        <div className='grid gap-8 lg:grid-cols-3'>
+          <div className='lg:col-span-2'>
+            <QuestionsList />
+          </div>
           <UserRating />
         </div>
-      </HomePageContainer>
+      </ScreenLg>
     </>
   );
 };
