44ff143d5e3a9de94162daaf93f826c94a05bad9
Ani Jujunashvili <ana.jujunashvili@gmail.com>
2024-12-02 04:38:59 +0400

    add tag selector in home page

--- src/pages/home-page/views/home-page.tsx
+++ src/pages/home-page/views/home-page.tsx
@@ -1,6 +1,7 @@
 import QuestionsList from './questions-list.tsx';
 import UserRating from './rating.tsx';
 import ScreenLg from '@/components/layout/page-containers/screen-lg.tsx';
+import TagSelector from './tag-selector.tsx';
 
 const HomePage = () => {
   return (
@@ -8,6 +9,7 @@ const HomePage = () => {
       <ScreenLg>
         <div className='grid gap-8 lg:grid-cols-3'>
           <div className='lg:col-span-2'>
+            <TagSelector />
             <QuestionsList />
           </div>
           <UserRating />
